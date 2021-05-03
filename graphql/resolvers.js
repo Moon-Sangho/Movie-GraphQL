9aa380b3a216161fcd2cb60e1855766330c50644
Moon-Sangho <skym887@gmail.com>
2021-05-03 11:20:35 +0900

    Wrapping a REST API with GraphQL

--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -1,35 +1,9 @@
-import { getMovies, getById, addMovie, deleteMovie } from "./db";
+import { getMovies } from "./db";
 
 const resolvers = {
   Query: {
-    movies: () => getMovies(),
-    movie: (_, { id }) => getById(id),
-  },
-  Mutation: {
-    addMovie: (_, { name, score }) => addMovie(name, score),
-    deleteMovie: (_, { id }) => deleteMovie(id),
+    movies: (_, { limit, rating }) => getMovies(limit, rating),
   },
 };
-
-// can check at localhost:4000 by writing like below code
-
-// query {
-//   movies {
-//     name
-//     score
-//   }
-// }
-
-// {
-//   movie(id: 1) {
-//     name
-//   }
-// }
-
-// mutation {
-//   addMovie(name: "hello", score: 1) {
-//     name
-//   }
-// }
 
 export default resolvers;
